b8f33ccb0f81d59aad2d6704126e4dcf7cb4b8e7
pietrobertino <pietrobertino20@gmail.com>
2026-03-24 15:13:42 +0100

    complete third milestone

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,8 +2,12 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 const api_url = "https://fakestoreapi.com/products"
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import BudgetContext from '../contexts/BudgetContext'
 
 export default function Products() {
+
+    const { budgetMode } = useContext(BudgetContext);
 
     const [products, setProducts] = useState([])
 
@@ -17,6 +21,11 @@ export default function Products() {
             })
     }, [])
 
+    function getBudgetProducts(list) {
+        const filteredList = list.filter(item => item.price <= 30)
+        return filteredList
+    }
+
     return (
 
         <div className="container">
@@ -25,26 +34,50 @@ export default function Products() {
             </h1>
 
             <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
-                {products.map(product => (
-                    <div className='col' key={product.id}>
-                        <div className="card p-3 h-100">
+                {
+                    budgetMode ?
+                        getBudgetProducts(products).map(product => (
+                            <div className='col' key={product.id}>
+                                <div className="card p-3 h-100">
 
-                            <div className=' card-header '>
-                                <div className='card-subtitle'>{product.category}</div>
-                                <img src={product.image} alt={product.title} className=' card-img p-3' />
-                            </div>
+                                    <div className=' card-header '>
+                                        <div className='card-subtitle'>{product.category}</div>
+                                        <img src={product.image} alt={product.title} className=' card-img p-3' />
+                                    </div>
 
-                            <div className='card-body d-flex flex-column'>
-                                <h3 className='card-title'>{product.title}</h3>
-                                <Link to={`/prodotti/${product.id}`}>Find out more</Link>
-                                <div className='card-footer mt-auto'>
-                                    <div className=' fw-bolder'>&pound;{product.price}</div>
+                                    <div className='card-body d-flex flex-column'>
+                                        <h3 className='card-title'>{product.title}</h3>
+                                        <Link to={`/prodotti/${product.id}`}>Find out more</Link>
+                                        <div className='card-footer mt-auto'>
+                                            <div className=' fw-bolder'>&pound;{product.price}</div>
+                                        </div>
+                                    </div>
+
                                 </div>
                             </div>
+                        ))
+                        :
+                        products.map(product => (
+                            <div className='col' key={product.id}>
+                                <div className="card p-3 h-100">
 
-                        </div>
-                    </div>
-                ))}
+                                    <div className=' card-header '>
+                                        <div className='card-subtitle'>{product.category}</div>
+                                        <img src={product.image} alt={product.title} className=' card-img p-3' />
+                                    </div>
+
+                                    <div className='card-body d-flex flex-column'>
+                                        <h3 className='card-title'>{product.title}</h3>
+                                        <Link to={`/prodotti/${product.id}`}>Find out more</Link>
+                                        <div className='card-footer mt-auto'>
+                                            <div className=' fw-bolder'>&pound;{product.price}</div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        ))
+                }
             </div>
         </div >
 
