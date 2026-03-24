bd69e9e3932f4d1831ae6a2bbcfeb1a66fedf595
pietrobertino <pietrobertino20@gmail.com>
2026-03-24 14:54:44 +0100

    complete second milestone

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,15 +1,20 @@
 import { NavLink } from "react-router-dom"
 import menu from "../data/Menu"
+import { useContext } from "react"
+import BudgetContext from "../contexts/BudgetContext"
 
 export default function AppHeader() {
+
+    const { setBudgetMode, budgetMode } = useContext(BudgetContext);
 
     return (
         <header>
             <div className="container">
-                <nav className="d-flex justify-content-center my-3">
+                <nav className="d-flex justify-content-center align-items-center my-3">
                     {menu.map(navItem => (
                         <NavLink key={navItem.id} to={navItem.path} className=" text-decoration-none px-3" id="nav-item">{navItem.text}</NavLink>
                     ))}
+                    <button onClick={() => setBudgetMode(!budgetMode)} className="ms-2">{budgetMode ? "Disattiva budget mode" : "Attiva budget mode"}</button>
                 </nav>
             </div>
         </header>
