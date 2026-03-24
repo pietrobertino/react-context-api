5f9916ce8a4ad3f7ab0a0ffe207da59e09b5d027
pietrobertino <pietrobertino20@gmail.com>
2026-03-24 14:41:22 +0100

    complete first milestone

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,28 @@ import Info from "./pages/Info"
 import Products from "./pages/Products"
 import Product from "./pages/Product"
 import NotFoundPage from "./pages/NotFoundPage"
+import BudgetContext from "./contexts/BudgetContext"
+import { useState } from "react"
 
 function App() {
 
+  const [budgetMode, setBudgetMode] = useState(false);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/chi-siamo" element={<Info />} />
-            <Route path="/prodotti" element={<Products />} />
-            <Route path="/prodotti/:id" element={<Product />} />
-            <Route path="/404-not-found" element={<NotFoundPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-
+      <BudgetContext.Provider value={{ budgetMode, setBudgetMode }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/chi-siamo" element={<Info />} />
+              <Route path="/prodotti" element={<Products />} />
+              <Route path="/prodotti/:id" element={<Product />} />
+              <Route path="/404-not-found" element={<NotFoundPage />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </BudgetContext.Provider>
     </>
   )
 }
